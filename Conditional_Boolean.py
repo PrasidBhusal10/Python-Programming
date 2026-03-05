1e265b3cb72b68ae28a8728198ceb84c967fc71f
PrasidBhusal10 <prasidbhusal07@gmail.com>
2026-03-05 17:49:06 -0600

    Add Conditional_Boolean.py with examples of conditional statements and boolean operations

---
+++ Conditional_Boolean.py
@@ -0,0 +1,30 @@
+lang = "java"
+if lang=="helo":
+    print("The condition is true")
+elif lang == "Prasid":
+    print("The condition is true")
+elif lang == "java":
+    print("You got it my friend")
+else:
+    print("The condition is false")
+
+#for boolean Operation
+x = "Hello"
+y = True
+if x == "Hello" and y:
+ print("Bool: You got it my friend")
+else:
+ print("Bool: You didnot my friend")
+
+ #False Values:
+ #false
+ #None
+ #0
+ # Any empty sequence, such as '', (), []
+ # Any empty mapping, such as {}
+ 
+x= ()
+if x:
+   print("This is true")
+else:
+    print("This is false") 
